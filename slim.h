b6bfbc80c63729316eca9bb697a4dda595a6cecb
不穿苦茶 <hhdbing3199167602@gmail.com>
2025-05-10 21:38:27 +0800

    Add files via upload

---
+++ slim.h
@@ -0,0 +1,10 @@
+#ifndef __SLIM_H
+#define __SLIM_H
+
+extern unsigned int highres_tick_ctrl;
+extern unsigned int highres_tick_ctrl_dbg;
+
+extern noinline int tracing_mark_write(const char *buf);
+
+
+#endif
